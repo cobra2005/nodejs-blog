caff247b4fc68d6d7e4087abdf672c0adf55b76e
unknown <you@example.com>
2024-11-24 15:38:37 +0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const morgan = require('morgan')
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
-  res.send("Minh Anh");
+app.use(morgan('combined'))
+
+app.get("/trang-chu", (req, res) => {
+  res.send(`
+        <h1 style="color: red">Minh Anh</h1>
+    `);
 });
 
 app.listen(port, () => {
