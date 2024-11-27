b0ea91ae0adadbf60939e10b70d4a8356f2e31df
unknown <you@example.com>
2024-11-27 11:25:11 +0700

    refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,13 +18,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/", (req, res) => {
-  res.render('home')
-});
+// Routes
 
-app.get("/news", (req, res) => {
-  res.render('news')
-});
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
