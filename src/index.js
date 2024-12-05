f8541fbb6c5d5ac8cfd91526042f447904fae019
unknown <you@example.com>
2024-12-05 20:36:10 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ const port = 3000;
 
 const route = require('./routes');
 
-
 // Middleware is used to parse data from the body of requests of type application/x-www-form-urlencoded(form data)
 app.use(express.urlencoded({ extended: true }));
-// Middleware is used to parse requests with bodies in JSON format 
+// Middleware is used to parse requests with bodies in JSON format
 app.use(express.json());
 
 // HTTP logger middleware - Morgan
@@ -21,12 +20,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Template engines - express-handlebars
 app.engine('hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+    app.set('view engine', '.hbs');
+        app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app);
+route(      app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
