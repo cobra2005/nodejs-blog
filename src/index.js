cb69ff5e2cb63c8c821988a38832bd92f69a722a
unknown <you@example.com>
2024-12-05 20:42:35 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars').engine;
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
 // Middleware is used to parse data from the body of requests of type application/x-www-form-urlencoded(form data)
 app.use(express.urlencoded({ extended: true }));
@@ -13,19 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTTP logger middleware - Morgan
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // Access to static file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Template engines - express-handlebars
-app.engine('hbs', handlebars({ extname: '.hbs' }));
-    app.set('view engine', '.hbs');
-        app.set('views', path.join(__dirname, 'resources/views'));
+app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 // Routes init
-route(      app);
+route(app);
 
-    app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
