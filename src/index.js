bbc35ba53dae1180a5dbea12706291f562b28136
unknown <you@example.com>
2024-12-05 18:28:52 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
+
+// Middleware is used to parse data from the body of requests of type application/x-www-form-urlencoded(form data)
+app.use(express.urlencoded({ extended: true }));
+// Middleware is used to parse requests with bodies in JSON format 
+app.use(express.json());
 
 // HTTP logger middleware - Morgan
 app.use(morgan('tiny'));
@@ -17,9 +24,8 @@ app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// Routes
-app.get('/trang-chu', (req, res) => res.render('home'));
-app.get('/news', (req, res) => res.render('news'));
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
